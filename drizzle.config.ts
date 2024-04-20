3b19ee2ffa912954e465dac9d13f13a3c7329803
Thinline20 <thinline20@gmail.com>
2024-04-20 22:56:40 +0900

    Add driver to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  driver: "better-sqlite",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
