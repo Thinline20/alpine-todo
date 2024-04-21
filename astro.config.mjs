714240db217ba2ecc20377bb4b0925ad10a30cc4
pekochan069 <pekochan069@gmail.com>
2024-04-22 02:03:02 +0900

    Add ssr capability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind()],
   vite: {
     optimizeDeps: {
-      exclude: ["oslo"],
-    },
+      exclude: ["oslo"]
+    }
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
