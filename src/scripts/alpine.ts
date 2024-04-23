f8f9ce46a675f3b7b03ff802845957c1d01b9547
pekochan069 <pekochan069@gmail.com>
2024-04-24 02:23:46 +0900

    Save work

--- src/scripts/alpine.ts
+++ src/scripts/alpine.ts
@@ -2,9 +2,11 @@ import Alpine from "alpinejs";
 import morph from "@alpinejs/morph";
 import persist from "@alpinejs/persist";
 import ajax from "@imacrayon/alpine-ajax";
+import Autosize from "@marcreichel/alpine-autosize";
 
 window.Alpine = Alpine;
 Alpine.plugin(morph);
 Alpine.plugin(persist);
 Alpine.plugin(ajax);
+Alpine.plugin(Autosize);
 Alpine.start();
