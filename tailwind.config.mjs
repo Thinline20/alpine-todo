470e62abdc16f998ed9fe99c53755d8e88acfc09
pekochan069 <pekochan069@gmail.com>
2024-04-26 02:42:53 +0900

    Add 1080p, 1440p screen breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ export default {
   darkMode: ["class", "[data-theme='dark']"],
   content: ["./src/**/*.{astro,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920px",
+        "4xl": "2560px",
+      }
+    },
   },
   plugins: [require("daisyui")],
 };
